51ededd2363387f3274e00de67171563d365daa9
hclivess <admin@bismuth.cz>
2022-08-30 01:45:12 +0200

    rename script

---
+++ rename.py
@@ -0,0 +1,10 @@
+import os
+import glob
+
+old_files = glob.glob("*.mp4_24libx265_aac128.mp4")
+print(old_files)
+
+for old_file in old_files:
+    new_name = old_file.replace(".mp4_24libx265_aac128", "")
+    os.rename(old_file, new_name)
+
